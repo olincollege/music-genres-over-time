928f2f1e71fb043347b5d59fd57cd4a2809f232a
Antoinette Tan <antoinette.tan.02@gmail.com>
2021-03-29 00:47:06 -0400

    Add files for creating bar chart race for genres.
    Add file for counting each genre per year in order to make bar chart race which animates the popular genres over time.

---
+++ count_genres_per_year.py
@@ -0,0 +1,79 @@
+"""
+Create two dictionaries that count the genres per year.
+count_genres_per_year counts the number of songs per genre in
+each year. accumulative_genre adds the number of songs per genre
+in each year to the amount of songs per genre for the years before
+it.
+"""
+
+import csv
+import ast
+
+
+def count_genres_per_year():
+    """
+    Returns a dictionary of the number of songs per genre in each year.
+
+    Returns a dictionary that has the year as the key and a dictionary
+    as the value. The inner dictionaries have genres as keys, and the
+    number of songs in each genre as the values.
+
+    RETURNS:
+        counted_genres: A dictionary with the value being an int representing
+        the year, and the key being another dictionary. This dictionary's key
+        has strings representing the genre, and ints representing the number
+        of songs in each genre for respective year.
+    """
+    counted_genres = {}
+    for year in range(1946, 2021):
+        counted_genres[year] = {}
+
+    with open('genre_year_data.csv', 'r') as data:
+        # goes through each song
+        for song in csv.DictReader(data):
+            # if there is no genre listed, skip to the next song
+            if song['Genre'] == '':
+                continue
+            year = int(song['Year'])
+            # getting rid of commas avoids creating two different list items for the one genre
+            song['Genre'] = song['Genre'].replace(
+                'Folk, World, & Country', 'Folk World & Country')
+            # makes string list into list. i.e. turns '['a','b']' to ['a','b']
+            song['Genre'] = ast.literal_eval(song['Genre'])
+            # adds one for each genre when it appears
+            for genre in song['Genre']:
+                counted_genres[year][genre] = counted_genres[year].get(
+                    genre, 0) + 1
+    return counted_genres
+
+
+def accumulative_genre():
+    """
+    Returns a dictionary of the cumulative number of songs per genre in each year.
+
+    Returns a dictionary that has the year as the key and a dictionary
+    as the value. The inner dictionaries have genres as keys, and the
+    cumulative number of songs in each genre as the values.
+
+    RETURNS:
+        accumulated_genres: A dictionary with the value being an int representing
+        the year, and the key being another dictionary. This dictionary's key
+        has strings representing the genre, and ints representing the cumulative
+        number of songs in each genre for respective year. i.e. if 1946 has 10 pop
+        songs, and 1947 has 15 pop songs, the cumulative number for 1947 is 25 pop
+        songs.
+    """
+    counted_genres = count_genres_per_year()
+    accumulated_genres = {}
+    # copying the dictionary key instead of setting it equal as to not change the original
+    # dictionary python does not implicitly copy objects, meaning that if I set them equal,
+    # both dictionaries would point to the same object
+    accumulated_genres[1946] = counted_genres[1946].copy()
+    for year in range(1947, 2021):
+        accumulated_genres[year] = accumulated_genres[year - 1].copy()
+        # add all genres from past year and current year
+        for genre in counted_genres[year]:
+            genre_count = accumulated_genres[year - 1].get(genre, 0)
+            accumulated_genres[year][genre] = genre_count + \
+                counted_genres[year][genre]
+    return accumulated_genres
---
+++ genres_bar_chart_race.py
@@ -0,0 +1,70 @@
+"""
+Creates bar chart race of the genres for the
+top songs from 1946 to 2020.
+"""
+
+import pandas as pd
+import bar_chart_race as bcr
+import count_genres_per_year
+
+def create_dataframe():
+    """
+    Creates a dataframe with the year as the index and genres as headers.
+
+    RETURNS:
+        genre_year_dataframe: A dataframe with the index being ints that
+        represent the years 1946 to 2020 and the headers being strings
+        that represent the most popular genres. In each column are ints
+        that represent the cumulative number of songs for each genre/year.
+
+    """
+    accumulated_genres = count_genres_per_year.accumulative_genre()
+    genre_year_dataframe = pd.DataFrame(accumulated_genres)
+    #switch rows and columns so that the columns are genres and the
+    #rows are the years
+    genre_year_dataframe = genre_year_dataframe.T
+    genre_year_dataframe.index.name = 'Year'
+    #fill any NaN with 0 instead
+    genre_year_dataframe.fillna(0, inplace = True)
+    return genre_year_dataframe
+
+
+def create_bar_chart_race():
+    """
+    Creates a bar chart race of the genres for the top songs over the years.
+
+    Creates a bar chart race that animates bars that represent the cumulative
+    number of top songs per genre over the years 1946 to 2020.
+    """
+
+    genre_year_dataframe = create_dataframe()
+    bcr.bar_chart_race(
+        df=genre_year_dataframe,
+        filename='popular_genres_over_time.mp4',
+        orientation='h',
+        sort='desc',
+        n_bars=10,
+        fixed_order=False,
+        fixed_max=False,
+        steps_per_period=10,
+        interpolate_period=False,
+        label_bars=True,
+        bar_size=.95,
+        period_label={'x': .99, 'y': .25, 'ha': 'right', 'va': 'center'},
+        period_fmt= 'Year{x:10.0f}',
+        period_length=500,
+        figsize=(5, 3),
+        dpi=144,
+        cmap='dark12',
+        title='Genres of the Top Songs from 1946 to 2020',
+        title_size='',
+        bar_label_size=7,
+        tick_label_size=7,
+        shared_fontdict={'family' : 'DejaVu Sans', 'color' : '.1'},
+        scale='linear',
+        writer=None,
+        fig=None,
+        bar_kwargs={'alpha': .7},
+        filter_column_colors=True)
+
+create_bar_chart_race()
